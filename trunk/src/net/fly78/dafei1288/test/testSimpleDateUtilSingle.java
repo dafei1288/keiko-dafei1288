eb540b2dfda98c21634ebe095019644c5433399d
dafei1288 <dafei1288@a98c159c-f06b-11dd-984d-ab748d24aa7e>
2009-04-01 16:55:24 +0000

    //ZIP功能 //mix Date操作类

--- trunk/src/net/fly78/dafei1288/test/testSimpleDateUtilSingle.java
+++ trunk/src/net/fly78/dafei1288/test/testSimpleDateUtilSingle.java
@@ -1,10 +1,12 @@
 package net.fly78.dafei1288.test;
 
 import java.text.ParseException;
+import java.util.Date;
 
 import junit.framework.TestCase;
 
 import net.fly78.dafei1288.util.date.SimpleDateUtilSingle;
+import net.fly78.dafei1288.util.date.DateOption;//.SimpleDateUtilSingle;
 
 public class testSimpleDateUtilSingle extends TestCase  {
 	public void testSingle(){
@@ -60,7 +62,7 @@ SimpleDateUtilSingle sdus = SimpleDateUtilSingle.getInstance("2008-11-2 12:22:55
 			System.out.println(sdus.addHour(-1115).toLocaleString());
 		*/
 			
-SimpleDateUtilSingle sdus = SimpleDateUtilSingle.getInstance("2008-11-2 12:22:55");
+			SimpleDateUtilSingle sdus = SimpleDateUtilSingle.getInstance("2008-11-2 12:22:55");
 			
 			System.out.println(sdus.getDate().toLocaleString());
 			
@@ -83,6 +85,13 @@ SimpleDateUtilSingle sdus = SimpleDateUtilSingle.getInstance("2008-11-2 12:22:55
 			System.out.println(sdus.addMins(1115).toLocaleString());
 			sdus = SimpleDateUtilSingle.getInstance("2008-11-2 12:22:55");
 			System.out.println(sdus.addMins(-1115).toLocaleString());
+			
+			Date t1 = new Date();
+			Date t2 = DateOption.str2Date("2009-3-20 17:22:55");
+			System.out.println("--->"+t1.equals(t2));
+			boolean t = DateOption.isBetween(DateOption.str2Date("2008-11-2 12:22:55"), DateOption.str2Date("2009-3-21 12:22:55"), new Date());
+			t = DateOption.isBefore(new Date(), DateOption.str2Date("2009-3-20 17:22:55"));
+			System.out.println(t);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();

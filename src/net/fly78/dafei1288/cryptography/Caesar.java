ef57952de502cb549a1bfc59d7e13eae37f99ca0
dafei1288 <dafei1288@a98c159c-f06b-11dd-984d-ab748d24aa7e>
2009-07-07 17:14:00 +0000

    添加cytography包 Caeser加密算法

---
+++ src/net/fly78/dafei1288/cryptography/Caesar.java
@@ -0,0 +1,59 @@
+package net.fly78.dafei1288.cryptography;
+
+import java.io.*;
+
+public class Caesar {
+	public static void encode(String PlainText, int Offset) {
+		String CipherText = "";
+		for (int i = 0; i < PlainText.length(); i++) {
+			if (PlainText.charAt(i) >= 'a' && PlainText.charAt(i) <= 'z')
+				CipherText += (char) ('a' + ((PlainText.charAt(i) - 'a' + Offset) % 26));
+			else if (PlainText.charAt(i) >= 'A' && PlainText.charAt(i) <= 'Z')
+				CipherText += (char) ('A' + ((PlainText.charAt(i) - 'A' + Offset) % 26));
+			else if (PlainText.charAt(i) >= '0' && PlainText.charAt(i) <= '9')
+				CipherText += (char) ('0' + ((PlainText.charAt(i) - '0' + Offset) % 10));
+			else 
+				CipherText += PlainText.charAt(i);
+		}
+		System.out.println("Ciphertext: " + CipherText);
+	}
+
+	public static void decode(String CipherText, int Offset) {
+		String PlainText = "";
+		for (int i = 0; i < CipherText.length(); i++) {
+			if (CipherText.charAt(i) >= 'a' && CipherText.charAt(i) <= 'z')
+				PlainText += (char) ('a' + ((CipherText.charAt(i) - 'a' - Offset) % 26));
+			else if (CipherText.charAt(i) >= 'A' && CipherText.charAt(i) <= 'Z')
+				PlainText += (char) ('A' + ((CipherText.charAt(i) - 'A' - Offset) % 26));
+			else if (CipherText.charAt(i) >= '0' && CipherText.charAt(i) <= '9')
+				PlainText += (char) ('0' + ((CipherText.charAt(i) - '0' - Offset) % 10));
+			else 
+				PlainText += CipherText.charAt(i);
+		}
+		System.out.println("PlainText: " + PlainText);
+	}
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					System.in));
+			System.out
+					.print("What to you want to do? (E)ncode or Decode?[e/d]");
+			String action = br.readLine();
+			System.out.print("Please input offset value: ");
+			int offset = Integer.valueOf(br.readLine());
+			System.out.print("Please input the ");
+			if (action.equalsIgnoreCase("d")) {
+				System.out.print("ciphertext: ");
+				decode(br.readLine(), offset);
+			} else {
+				System.out.print("plaintext: ");
+				encode(br.readLine(), offset);
+			}
+			System.out.println("Press ENTER...");
+			System.in.read();
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+}
